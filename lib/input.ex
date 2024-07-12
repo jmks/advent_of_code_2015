c9e57c9f32c7a1f9504d5c0a497ead56290875e4
Jason Schweier <jason.schweier@gmail.com>
2024-07-11 20:22:55 -0400

    Extract function

--- lib/input.ex
+++ lib/input.ex
@@ -1,15 +1,21 @@
 defmodule Input do
   def read(day) do
     day
-    |> to_string()
-    |> String.pad_leading(2, "0")
+    |> stringified_day()
     |> then(fn day ->
-      File.read!("lib/day_#{day}/input")
+      File.read!("lib/#{day}/input")
     end)
   end
 
   def lines(day) do
     read(day)
     |> String.split("\n")
+  end
+
+  def stringified_day(day) do
+    day
+    |> to_string()
+    |> String.pad_leading(2, "0")
+    |> then(fn d -> "day_#{d}" end)
   end
 end
