e3a5e9718b87f7e6ad67032e8fb3f4b6cb929918
Jason Schweier <jason.schweier@gmail.com>
2024-07-12 09:58:48 -0400

    Day 04 - part 2

--- lib/day_04/stocking_stuffer.ex
+++ lib/day_04/stocking_stuffer.ex
@@ -1,17 +1,17 @@
 defmodule Day04.StockingStuffer do
-  def decimal(secret_key) do
-    do_decimal(secret_key, 1)
+  def decimal(secret_key, leading_zeros \\ 5) do
+    do_decimal(secret_key, leading_zeros, 1)
   end
 
-  defp do_decimal(secret, number) do
+  defp do_decimal(secret, zeros, number) do
     hash =
       :crypto.hash(:md5 , secret <> to_string(number))
       |> Base.encode16()
 
-    if String.starts_with?(hash, "00000") do
+    if String.starts_with?(hash, String.duplicate("0", zeros)) do
       number
     else
-      do_decimal(secret, number + 1)
+      do_decimal(secret, zeros, number + 1)
     end
   end
 end
